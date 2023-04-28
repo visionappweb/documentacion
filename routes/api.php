6b83de4254a2fd57574b9f42fe8a0de52dfdbc8d
kevin_dev <kevinvillalba.p@gmail.com>
2023-04-28 12:46:39 -0500

    DOC-1 Desarrollo api para modulo universitario.

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,10 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::prefix('universitario')->group(function () {
+    Route::prefix('estudiantes')->group(function () {
+        Route::get('estudiante', [\App\Http\Controllers\EstudianteController::class, 'searchWhereId']);
+        Route::post('estudiante', [\App\Http\Controllers\EstudianteController::class, 'store']);
+    });
+});
